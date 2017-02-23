39f1793a83acfba1cc1168f39fa35b11b8904614
Brad Westfall <brad@azpixels.com>
2017-02-23 08:44:42 -0700

    more work on CSS and Home page

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,8 +1,17 @@
-(function($, undefined) {
+;(function($, undefined) {
   $(document).ready(function() {
 
     $('.hero').slick({
       dots: true
+    })
+
+    $('.quotes').slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      fade: true,
+      speed: 2000,
+      arrows: false
     })
 
     $('.panel.services .service').on('click', function() {
