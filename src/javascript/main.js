47f40ec770db7bc991c0fc0c1ba493d5677b9e07
Brad Westfall <brad@azpixels.com>
2017-03-20 23:34:00 -0700

    tons of new changes

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,9 +1,12 @@
 ;(function($, undefined) {
   $(document).ready(function() {
 
+    var quotes = $('.quotes .quote').length
+
     $('.hero').slick({
-      dots: true
-    })
+      dots: true,
+      arrows: false
+    });
 
     $('.quotes').slick({
       infinite: true,
@@ -11,12 +14,22 @@
       autoplaySpeed: 10000,
       fade: true,
       speed: 2000,
-      arrows: false
-    })
+      arrows: false,
+      initialSlide: Math.floor(Math.random() * quotes)
+    });
 
     $('.panel.services .service').on('click', function() {
-      $('.hero').slick('slickGoTo', $(this).index())
+      $('.hero').slick('slickGoTo', $(this).index());
+    });
+
+    $('.toggle-primary-nav').on('click', function(e) {
+      e.stopPropagation();
+      $(this).toggleClass('active');
+    });
+
+    $(window).on('click', function() {
+      $('.toggle-primary-nav').removeClass('active');
     })
 
-  })
+  });
 })(jQuery)
