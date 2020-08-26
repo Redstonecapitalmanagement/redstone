76381cbd17e0b17a61a745ffeaaa89ca0592c0d4
Brad Westfall <brad@azpixels.com>
2020-08-26 16:33:36 -0700

    getting rid of gulp

---
+++ README.md
@@ -0,0 +1,9 @@
+Make sure to `npm i harp -g`
+
+```sh
+# Dev
+harp server src
+
+# Build
+harp compile src dist
+```
---
+++ dist/javascript/main.js
@@ -0,0 +1,40 @@
+;(function($, undefined) {
+  $(document).ready(function() {
+
+    var quotes = $('.quotes .quote').length
+
+    $('.hero').slick({
+      dots: true,
+      arrows: false
+    });
+
+    $('.quotes').slick({
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 10000,
+      fade: true,
+      speed: 2000,
+      arrows: false,
+      initialSlide: Math.floor(Math.random() * quotes)
+    });
+
+    $('.panel.services .service').on('click', function() {
+      $('.hero').slick('slickGoTo', $(this).index());
+    });
+
+    $('.toggle-primary-nav').on('click', function(e) {
+      e.stopPropagation();
+      $(this).toggleClass('active');
+    });
+
+    $(window).on('click', function() {
+      $('.toggle-primary-nav').removeClass('active');
+    });
+
+    $.get('data.json')
+      .then(function(data) {
+        console.log(data.comentary[0])
+      })
+
+  });
+})(jQuery)
